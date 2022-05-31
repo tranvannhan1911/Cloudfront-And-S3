b83440864d5ba64a3072cf20f3113e106a48583a
tranvannhan1911 <tranvannhan1911@gmail.com>
2022-05-31 18:34:38 +0700

    init spring boot app

---
+++ src/main/java/com/example/cloudfront_s3/CloudfrontS3Application.java
@@ -0,0 +1,13 @@
+package com.example.cloudfront_s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudfrontS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudfrontS3Application.class, args);
+	}
+
+}
